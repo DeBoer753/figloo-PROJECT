0ed4bb0512aa5e8bf2e4f2036bbf2c4bfff56213
Myles DeBoer <myles.deboer@gmail.com>
2022-10-31 16:51:47 -0700

    News: Successfully integrated market news API into their own cards.

--- src/routes/News.jsx
+++ src/routes/News.jsx
@@ -1,30 +1,51 @@
 // API KEY (marketaux - Finance News): 9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl
-// API LINK (marketaux - Fetch link for NYSE data):  https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&limit=10&published_after=2022-10-25T18:44&api_token=9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl
+// API LINK (marketaux - Fetch link for NYSE data):  
+// API TEST LINK (Free) - Cat Facts: https://catfact.ninja/facts?max_length=30&limit=10
 
 // REACT:
-import {React, useState, useEffect} from 'react'
+import {React, useState, useEffect, Link} from 'react'
+
+// CSS: 
+import styles from '../css/News.module.css'
 
 // HOME:
 export default function News() {
-  const [nyseData, setNyseData] = useState('')
+  //State:
+  const [nyseData, setNyseData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
+  // Fetched: (finance)
   useEffect(() => {
-    fetch(`https://catfact.ninja/facts?max_length=30&limit=10`) // Test: Cat API (replace with API LINK above when ready)
+    fetch(`https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&limit=10&published_after=2022-10-25T18:44&api_token=9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      setNyseData(data)
-  });
+      setNyseData(data.data) // Tip: set data to an array if you want to use map method in return statement.
+      setIsLoading(false)
+    });
+    // .catch(error => console.error('data not loaded', error)) ••• issue with this line.
   }, [])
-  
-  // console.log(nyseData);
-  // {nyseData.data[0].fact} 
+
+  console.log('State Data:', nyseData)
 
   return (
     <>
-      <div>
-        <h1>{nyseData.to}</h1>
+      <div className={styles.mainBox}>
+        {/* {isLoading ? <h1>loading...</h1> : <h1>{nyseData.data[0].fact}</h1>} */}
+        {isLoading ? <h1>loading...</h1> :
+        <div className={styles.stockMarketBox}>
+          {nyseData.map((x, i) => { 
+            return (
+              <div key={i} className={styles.stockMarketCards}>
+                <h1>{x.title}</h1>
+                <img src={x.image_url}></img>
+                <h2>{x.description}</h2>
+                <a href={x.url} target={'_blank'} ><h3>Full Article</h3></a>
+              </div>
+            )
+          })}
+          </div>}
       </div>
     </>
   )
 }
+
