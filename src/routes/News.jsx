5bb560a4c651b1d1cab4515fefa29a78c54a4750
Myles DeBoer <myles.deboer@gmail.com>
2022-11-03 12:19:11 -0700

    News: added new API fetches. Planning to swap out crytpo and real estate apis for different source.

--- src/routes/News.jsx
+++ src/routes/News.jsx
@@ -1,5 +1,5 @@
-// API KEY (marketaux - Finance News): 9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl
-// API LINK (marketaux - Fetch link for NYSE data):  
+// API KEY (marketaux): 9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl
+// API KEY (cyrptopanic): c9e9aaa5978350bc96299915d63c9647e10e2a89
 // API TEST LINK (Free) - Cat Facts: https://catfact.ninja/facts?max_length=30&limit=10
 
 // REACT:
@@ -12,20 +12,46 @@ import styles from '../css/News.module.css'
 export default function News() {
   //State:
   const [nyseData, setNyseData] = useState([])
+  const [cryptoData, setCryptoData] = useState([])
+  const [realEstateData, setRealEstateData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  // Fetched: (finance)
-  useEffect(() => {
-    fetch(`https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&limit=10&published_after=2022-10-25T18:44&api_token=9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl`)
-    .then(res => res.json())
-    .then(data => {
-      setNyseData(data.data) // Tip: set data to an array if you want to use map method in return statement.
-      setIsLoading(false)
-    });
-    // .catch(error => console.error('data not loaded', error)) ••• issue with this line.
-  }, [])
+  // // Fetched: (finance)
+  // useEffect(() => {
+  //   fetch(`https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&limit=10&published_after=2022-10-25T18:44&api_token=9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setNyseData(data.data) // Tip: set data to an array if you want to use map method in return statement.
+  //     setIsLoading(false)
+  //   });
+  //  // .catch(error => console.error('data not loaded', error)) ••• issue with this line.
+  // }, [])
+  // console.log('Market Fetched Data:', nyseData)
 
-  console.log('State Data:', nyseData)
+  // // Fetched: (crypto) ••• Not actually crypto. find new api link
+  // useEffect(() => {
+  //   fetch(`https://crypto-news15.p.rapidapi.com/news/amb`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log('TEST: ', data);
+  //     setCryptoData(data.data) // Tip: set data to an array if you want to use map method in return statement.
+  //     setIsLoading(false)
+  //   });
+  //   // .catch(error => console.error('data not loaded', error)) ••• issue with this line.
+  // }, [])
+  // console.log('Crypto Fetched Data:', cryptoData)
+
+  // // Fetched: (real estate) ••• Consider new api link
+  // useEffect(() => {
+  //   fetch(`https://api.marketaux.com/v1/news/all?industries=Real Estate&filter_entities=true&limit=10&published_after=2022-11-01T18:45&api_token=9cMPFNNDa0hmlvpRyLyf8sPU01pdB3HtScUqhytl`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setRealEstateData(data.data) // Tip: set data to an array if you want to use map method in return statement.
+  //     setIsLoading(false)
+  //   });
+  //   // .catch(error => console.error('data not loaded', error)) ••• issue with this line.
+  // }, [])
+  // console.log('Real Estate Fetched Data:', realEstateData)
 
   return (
     <>
@@ -33,15 +59,17 @@ export default function News() {
         {/* {isLoading ? <h1>loading...</h1> : <h1>{nyseData.data[0].fact}</h1>} */}
         <div className={styles.secondaryBox}>
           <h1>Market</h1>
-          {isLoading ? <h1>loading...</h1> :
-          <div className={styles.stockMarketBox}>
+          {isLoading ? <></> :
+          <div className={styles.stockMarketBox}> 
             {nyseData.map((x, i) => { 
               return (
                 <div key={i} className={styles.stockMarketCards}>
                   <h2>{x.title}</h2>
                   <img src={x.image_url}></img>
-                  <h3>{x.description}</h3>
-                  <a href={x.url} target={'_blank'} ><h4>Full Article</h4></a>
+                  <p>{x.description}</p>
+                  <div>
+                    <a href={x.url} target={'_blank'} ><h4>Read More</h4></a>
+                  </div>
                 </div>
               )
             })}
@@ -50,15 +78,15 @@ export default function News() {
         {/* {isLoading ? <h1>loading...</h1> : <h1>{nyseData.data[0].fact}</h1>} */}
         <div className={styles.secondaryBox}>
           <h1>Crypto</h1>
-          {isLoading ? <h1>loading...</h1> :
+          {isLoading ? <></> :
           <div className={styles.stockMarketBox}>
-            {nyseData.map((x, i) => { 
+            {cryptoData.map((x, i) => { 
               return (
                 <div key={i} className={styles.stockMarketCards}>
                   <h2>{x.title}</h2>
                   <img src={x.image_url}></img>
-                  <h3>{x.description}</h3>
-                  <a href={x.url} target={'_blank'} ><h4>Full Article</h4></a>
+                  <p>{x.description}</p>
+                  <a href={x.url} target={'_blank'} ><h4>Read More</h4></a>
                 </div>
               )
             })}
@@ -67,15 +95,15 @@ export default function News() {
         {/* {isLoading ? <h1>loading...</h1> : <h1>{nyseData.data[0].fact}</h1>} */}
         <div className={styles.secondaryBox}>
           <h1>Real Estate</h1>
-          {isLoading ? <h1>loading...</h1> :
+          {isLoading ? <></> :
           <div className={styles.stockMarketBox}>
-            {nyseData.map((x, i) => { 
+            {realEstateData.map((x, i) => { 
               return (
                 <div key={i} className={styles.stockMarketCards}>
                   <h2>{x.title}</h2>
                   <img src={x.image_url}></img>
-                  <h3>{x.description}</h3>
-                  <a href={x.url} target={'_blank'} ><h4>Full Article</h4></a>
+                  <p>{x.description}</p>
+                  <a href={x.url} target={'_blank'} ><h4>Read More</h4></a>
                 </div>
               )
             })}
@@ -86,3 +114,11 @@ export default function News() {
   )
 }
 
+/* ADD:
+- automatically convert foreign articles to english 
+- if no photo attached to article replace with something else
+- THOUGHT: if article holds key words, change photo to custom photo using photoshop? 
+*/
+
+/* QUESTIONS:
+*/
